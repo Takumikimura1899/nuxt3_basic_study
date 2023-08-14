d1e26e0da9549b03ecaadfbc0d367d9be5ae9d3c
Takumikimura1899 <kimura141899@gmail.com>
2023-08-14 18:58:38 +0900

    assets icon

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <h1>Main Page</h1>
-    <button @click="handleClick">Coupon Get</button>
-    <LazyCoupon v-if="show" />
+    <div>
+      <img src="~/assets/icon.png" alt="Nuxt3 Icon" />
+    </div>
   </div>
 </template>
-<script setup>
-const show = ref(false);
-const handleClick = () => {
-  show.value = true;
-};
-</script>
