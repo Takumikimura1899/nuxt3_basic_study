b1ad9f1f0ff75bbd3391602dfcbb09f804c82806
Takumikimura1899 <kimura141899@gmail.com>
2023-08-14 18:31:02 +0900

    v-if show

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Main Page</h1>
+  <div>
+    <h1>Main Page</h1>
+    <button @click="handleClick">Coupon Get</button>
+    <Coupon v-if="show" />
+  </div>
 </template>
+<script setup>
+const show = ref(false);
+const handleClick = () => {
+  show.value = true;
+};
+</script>
