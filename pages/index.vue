cb7f0dd66dd709407e0d0987aa5f9e30f15cbb4d
Takumikimura1899 <kimura141899@gmail.com>
2023-08-14 18:07:10 +0900

    navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,3 @@
 <template>
-  <div>
-    <NuxtLayout name="default">
-      <template #header>ヘッダー</template>
-      <h1>Main Page</h1>
-    </NuxtLayout>
-  </div>
+  <h1>Main Page</h1>
 </template>
-<script setup>
-definePageMeta({
-  layout: false,
-});
-</script>
