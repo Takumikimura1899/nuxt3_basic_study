4644d47d61213a906bec1ef4e5297005a515231c
Takumikimura1899 <kimura141899@gmail.com>
2023-08-16 19:14:30 +0900

    composables it correspondes to customhook in react

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <h1>Main Page</h1>
+    <div>Count:{{ count }}</div>
     <div>
-      <img src="/icon.png" alt="Nuxt3 Icon" />
+      <button @click="() => inc()">increase</button>
+      <button @click="() => dec()">decrease</button>
     </div>
   </div>
 </template>
+
+<script setup>
+const { count, inc, dec } = useCounter(100);
+</script>
