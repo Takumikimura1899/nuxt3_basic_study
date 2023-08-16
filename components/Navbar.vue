fae55a75711cc910a9720594496d0ddbeb77a8b2
Takumikimura1899 <kimura141899@gmail.com>
2023-08-17 08:20:10 +0900

    routing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,6 +2,6 @@
   <nav>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
-    <NuxtLink to="https://google.com" target="_blank"> Google </NuxtLink>
+    <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink>
   </nav>
 </template>
