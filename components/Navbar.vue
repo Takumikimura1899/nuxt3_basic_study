eb3977fa4fb94b413157e36ada24337146bc3777
Takumikimura1899 <kimura141899@gmail.com>
2023-08-19 07:57:15 +0900

    useFetch and refresh

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,5 +3,6 @@
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/about">About</NuxtLink>
     <NuxtLink :to="{ name: 'users-list' }">User List</NuxtLink>
+    <NuxtLink to="/posts">User List</NuxtLink>
   </nav>
 </template>
